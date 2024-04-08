be825f9cdbed5bc98258c97be022bf4286044701
JuanPabloFajardoSocha <fajardop662@gmail.com>
2024-04-08 14:57:03 -0500

    first commit

---
+++ RickAndMorty/Entities/Characters.cs
@@ -0,0 +1,18 @@
+﻿
+namespace RickAndMorty.Entities
+{
+    public class Characters
+    {
+        public Info info {  get; set; } 
+        public List<Character> results {  get; set; }   
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+
+        public string prev { get; set; }   
+    }
+}
